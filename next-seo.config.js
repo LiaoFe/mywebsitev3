3f6aee57c02661403897432ad6e01a63ee28cfd2
felix <f4liao@uwaterloo.ca>
2023-10-02 00:49:58 -0400

    error fixes

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,31 +1,9 @@
 export default {
-  title: "ironsoul | Temirzhan Yussupov",
+  title: "Felix ",
   description: "Real full-stack ninja lives here.",
-  canonical: "https://ironsoul.lol",
   openGraph: {
-    url: "https://ironsoul.lol",
-    title: "ironsoul | Temirzhan Yussupov",
-    description: "Real full-stack ninja lives here.",
-    images: [
-      {
-        url: "https://ironsoul.lol/og-image-01.png",
-        width: 800,
-        height: 600,
-        alt: "Og Image Alt",
-      },
-      {
-        url: "https://ironsoul.lol/og-image-02.png",
-        width: 800,
-        height: 800,
-        alt: "Og Image Alt 2",
-      },
-      {
-        url: "https://ironsoul.lol/og-image-03.png",
-        width: 900,
-        height: 800,
-        alt: "Og Image Alt 3",
-      },
-    ],
-    site_name: "ironsoul | Temirzhan Yussupov",
+    title: "Felix ",
+    description: "Real full-stack wizards lives here.",
+    site_name: "Felix ",
   },
 };
